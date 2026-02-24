8ef21b8a5d361af7c8e7212fd333f8ab3eb1a0a2
Shreeraksha H <shreeraksha.h@mphasis.com>
2026-02-24 16:52:18 +0530

    spring core

---
+++ SpringCoreExp2/src/main/java/com/test/MainTest.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+	public static void main(String[] args) {
+//		ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
+//		Customer obj=ctx.getBean("cst1", Customer.class);
+//		System.out.println(obj);
+		ApplicationContext ctx= new AnnotationConfigApplicationContext(AnnoConfig.class);
+		Address aobj=ctx.getBean(Address.class);
+		aobj.setCity("Tirumala");
+		aobj.setState("AP");
+		aobj.setCountry("IN");
+		
+		Customer cst=ctx.getBean(Customer.class);
+		
+		cst.setId(102);
+		cst.setName("Sky");
+		cst.setAdr(aobj);
+		
+		System.out.println(cst);
+	}
+}
