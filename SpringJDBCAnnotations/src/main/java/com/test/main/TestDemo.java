acfb5d729ac30079998e6aaa402d2ea455227792
Shreeraksha H <shreeraksha.h@mphasis.com>
2026-02-25 17:44:03 +0530

    Spring JDBC

---
+++ SpringJDBCAnnotations/src/main/java/com/test/main/TestDemo.java
@@ -0,0 +1,29 @@
+package com.test.main;
+
+import java.awt.Frame;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.test.config.DBConfig;
+import com.test.dao.FruitsDao;
+import com.test.model.Fruits;
+
+public class TestDemo {
+	public static void main(String[] args) {
+	
+	ApplicationContext ctx=new AnnotationConfigApplicationContext(DBConfig.class);
+	FruitsDao dao=ctx.getBean(FruitsDao.class);
+	Fruits f =new Fruits();
+	f.setId(201);
+	f.setName("Grapes");
+	f.setPrice(100);
+	f.setQuantity(15);
+	
+	dao.saveFruits(f);
+	
+	List<Fruits> list=dao.getAllFruits();
+	list.forEach(System.out::println);
+	}
+}
