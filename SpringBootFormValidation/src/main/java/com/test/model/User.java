a23333d99eb6bd2e2bf881f9623512be74f9830e
Shreeraksha H <shreeraksha.h@mphasis.com>
2026-02-26 17:29:35 +0530

    Spring form validation

---
+++ SpringBootFormValidation/src/main/java/com/test/model/User.java
@@ -0,0 +1,27 @@
+package com.test.model;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class User {
+	@NotEmpty(message="username required")
+	private String username;
+	
+	@NotEmpty(message = "Password required")
+	@Size(min=9, max=15)
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
